28568b559654bd1fe661669cfa2e7b2ca1056fe0
ShawnHosea <shawnhosea@shawnhosea.com>
2020-09-22 17:28:19 -0400

    img2

--- src/views/header.js
+++ src/views/header.js
@@ -8,7 +8,7 @@ module.exports = function Header(props) {
 return `
     <div class='Header'>
         <div class='HeaderGroup'>
-            <a href="/"><img height='40px' alt='logo' src="${arc.static(/_static/logo.png)}"/></a>
+            <a href="/"><img height='40px' alt='logo' src="${arc.static('/_static/logo.png')}"/></a>
             <a href="/about">About</a>
             <a href="/contact">Contact</a>
             <button>Login</button>
