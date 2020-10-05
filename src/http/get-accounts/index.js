0e5e60f4f5d952840015dec2e5571a2752f591fd
ShawnHosea <shawnhosea@shawnhosea.com>
2020-10-04 21:28:43 -0400

    runDestroy

--- src/http/get-accounts/index.js
+++ src/http/get-accounts/index.js
@@ -1,17 +1,16 @@
 const data = require('@begin/data')
 
 exports.handler = async function accounts (req) {
+    
   let accounts = await data.get({
     table: 'accounts'
   })
+
   // Return oldest account first
   accounts.sort((a, b) => a.created > b.created)
 
   let table = 'accounts'
   let tableCount = await data.count({table})
-  console.log(table)
-  console.log(accounts)
-  console.log(tableCount)
 
   return {
     statusCode: 201,
--- src/http/post-signup-delete/index.js
+++ src/http/post-signup-delete/index.js
@@ -4,19 +4,27 @@ let data = require('@begin/data')
 
 exports.handler = async function destroy (req) {
   // let key = arc.http.helpers.bodyParser(req).key // Base64 decodes + parses body
-  // console.log(key)
   
-  await data.destroy({
-    table: 'accounts',
-    key: ''
-    
-  })
+  let table = 'accounts'
+  let tableCount = await data.count({table})
+
+  let run = () => {
+    if (tableCount <= 0)
+    then
+    await data.destroy({
+      table: 'accounts',
+      key: 'Y5KxlnRZuD'
+      
+    })
+  }
+  run()
 
   return {
     statusCode: 302,
     headers: {
       location: '/',
       'cache-control': 'no-cache, no-store, must-revalidate, max-age=0, s-maxage=0'
-    }
+    },
+    
   }
 }
