7c29e275549459fa2b4cd322827f149bfb92dce8
ShawnHosea <shawnhosea@shawnhosea.com>
2020-10-05 02:14:36 -0400

    key

--- src/http/post-signup-delete/index.js
+++ src/http/post-signup-delete/index.js
@@ -5,29 +5,16 @@ let data = require('@begin/data')
 exports.handler = async function destroy (req) {
   // let key = arc.http.helpers.bodyParser(req).key // Base64 decodes + parses body
   
-  let accounts = await data.get({
-    table:'accounts'
-})
 
-console.log(accounts)
-
-//   // Return oldest account first
-//   table.sort((a, b) => a.created > b.created)
-
-let table = 'accounts'
-
-let tableCount = await data.count({table})
-console.log(tableCount)
-  
 
   await data.destroy([
     {
     table: 'accounts',
-    key: 'VQEyW70lsP'
+    key: 'nOKR778Nhm'
     },
     {
     table: 'accounts',
-    key: 'l7r279nlCA'
+    key: 'l7GRXqQptP'
     }
 ])
   
