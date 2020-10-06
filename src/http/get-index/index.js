7dd42777a3d7de128dc7418bdc315a172b7a4b0b
ShawnHosea <shawnhosea@shawnhosea.com>
2020-10-06 09:03:08 -0400

    update

--- src/http/get-index/index.js
+++ src/http/get-index/index.js
@@ -6,25 +6,29 @@ exports.handler = async function Index (req) {
 
   let body = Layout({ 
     content: 
-    `<div>
-        <div class="">
+    `<div class="wrapper">
+        <div class="m-auto block-lg text-center mt1">
           <a href="signup" class="uppercase no-underline pt-1 pb-1 pr2 pl2 bg-g10 text-g0 radius0 cursor-pointer bg-h5 text-h2 bg-d1 m0">Create new account</a>
         </div>
-        <div class="mt1">
-          <a href="/accounts"
-          class="uppercase no-underline pt-1 pb-1 pr2 pl2 bg-g10 text-g0 radius0 cursor-pointer bg-h5 text-h2 bg-d1 m0">Check Accounts</a>
-        </div>
-    </div>
 
-    <div>
+        <div class="divider">
+          <strong class="divider-title ng-binding">OR</strong>
+        </div>
+
+        <h3 class="text-center m2">For Developer testing 👇🏾</h3>
+        <div class="m-auto block-lg text-center mt1">
+          <a href="/accounts"
+          class="uppercase no-underline pt-1 pb-1 pr2 pl2 bg-g10 text-g0 radius0 cursor-pointer bg-h5 text-h2 bg-d1 ">Check Accounts</a>
+        </div>
+   
+
+    <div class="m-auto block-lg text-center mt1">
       <form action="/signup/delete" method="POST">
-          <div>
-              <button 
-              class="uppercase no-underline pt-1 pb-1 pr2 pl2 bg-g10 text-g0 radius0 cursor-pointer bg-h5 text-h2 bg-d1 m1" 
-              type="submit">Delete Accounts</button>
-          </div>
+         <button 
+           class="uppercase no-underline pt-1 pb-1 pr2 pl2 bg-g10 text-g0 radius0 cursor-pointer bg-h5 text-h2 bg-d1 m1" type="submit">Delete Accounts</button>
       </form>
     </div>
+  </div>
     ` 
   })
 
